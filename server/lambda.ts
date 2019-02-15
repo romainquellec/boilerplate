7791d264875bf2fafd794134fd520f47dd7016de
Romain Quellec <romain.quellec@gmail.com>
2019-02-15 11:11:35 +0100

    update boilerplate

--- server/lambda.ts
+++ server/lambda.ts
@@ -2,31 +2,24 @@ const compression = require("compression");
 const express = require("express");
 const { parse } = require("url");
 const serverless = require("serverless-http");
+const pathMatch = require("path-match");
 const isProduction = process.env.NODE_ENV === "production";
 
 // setup Express and hook up Next.js handler
 const app = express();
 app.use(compression());
 
-// tslint:disable-next-line
-const pathMatch = require("path-match");
-
-const getPage = page => require(`./.next/serverless/pages/${page}`).render;
 const route = pathMatch();
-const matches = [
-  { route: route("/"), page: "/home" },
-  { route: route("/workshops/:id/edit"), page: "/workshop-edit" },
-  { route: route("/workshops/new"), page: "/workshop-new" },
-  { route: route("/workshops/:id"), page: "/workshop" },
-];
+const matches = [];
 
 // for local development (serverless offline)
 if (!isProduction) {
   // host the static files
-  app.use("/_next/static", express.static("./.next/static"));
+  app.use("/_next/static", express.static("./static"));
   app.use("/static", express.static("./static"));
 }
 
+app.get('/', require('./serverless/pages/index').render)
 app.get('*', (req, res) => {
 
   const parsedUrl = parse(req.url, true);
@@ -37,9 +30,9 @@ app.get('*', (req, res) => {
     const params = match.route(pathname);
     if (params) {
       try {
-        require(`./.next/serverless/pages${pathname}`).render(req, res, match.page, Object.assign(params, query))
+        require(`./serverless/pages${pathname}`).render(req, res, match.page, Object.assign(params, query))
       } catch (err) {
-        require('./.next/serverless/pages/_error').render(req, res, match.page, Object.assign(params, query))
+        require('./serverless/pages/_error').render(req, res, match.page, Object.assign(params, query))
       }
       hasMatch = true;
       break;
@@ -47,15 +40,15 @@ app.get('*', (req, res) => {
   }
   if (!hasMatch) {
     try {
-      require(`./.next/serverless/pages${pathname}`).render(req, res, parsedUrl)
+      require(`./serverless/pages${pathname}`).render(req, res, parsedUrl)
     } catch (err) {
-      require('./.next/serverless/pages/_error').render(req, res, parsedUrl)
+      require('./serverless/pages/_error').render(req, res, parsedUrl)
     }
   }
 })
 
 // 404 handler
-app.get("*", require('./.next/serverless/pages/_error').render);
+app.get("*", require('./serverless/pages/_error').render);
 
 // export the wrapped handler for the Lambda runtime
 exports.handler = serverless(app);
