092164fcbcf9021aca681004474e877963ddf273
Romain Quellec <romain.quellec@gmail.com>
2019-04-23 11:09:15 +0200

    Use serverless-nextjs-plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.STAGE === 'production'
 const withTypescript = require('@zeit/next-typescript')
 
 const nextConfig = {
+  assetPrefix: isProd ? 'https://assets.xxx.com' : '',
   target: 'serverless',
   distDir: 'build',
   generateBuildId: async () => {
